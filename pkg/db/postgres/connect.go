54d3d5ce0ab42a7d1c23d9da469ea93559903186
RyabikovAlex <ryabikov.alex@gmail.com>
2023-04-22 15:01:21 +0300

    feat: Added database module (postgres)

---
+++ pkg/db/postgres/connect.go
@@ -0,0 +1,40 @@
+package postgres
+
+import (
+	"Diploma/config"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+const (
+	maxOpenConns    = 60
+	connMaxLifetime = 120
+	maxIdleConns    = 30
+	connMaxIdleTime = 20
+)
+
+// Return new Postgresql db instance
+func NewPsqlDB(c *config.Config) (*sql.DB, error) {
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		c.Postgres.PostgresqlHost,
+		c.Postgres.PostgresqlPort,
+		c.Postgres.PostgresqlUser,
+		c.Postgres.PostgresqlDbname,
+		c.Postgres.PostgresqlPassword,
+	)
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxLifetime(connMaxLifetime * time.Second)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxIdleTime(connMaxIdleTime * time.Second)
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
