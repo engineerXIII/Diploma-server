c57144ff19b38dc15cc49039d6dfee260f0109e6
RyabikovAlex <ryabikov.alex@gmail.com>
2023-04-22 15:00:58 +0300

    feat: Added config module

---
+++ config/config.go
@@ -0,0 +1,117 @@
+package config
+
+import (
+	"errors"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// App config struct
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+	Cookie   Cookie
+	Store    Store
+	Session  Session
+	Metrics  Metrics
+	Logger   Logger
+}
+
+// Server config struct
+type ServerConfig struct {
+	AppVersion        string
+	Port              string
+	PprofPort         string
+	Mode              string
+	JwtSecretKey      string
+	CookieName        string
+	ReadTimeout       time.Duration
+	WriteTimeout      time.Duration
+	SSL               bool
+	CtxDefaultTimeout time.Duration
+	CSRF              bool
+	Debug             bool
+}
+
+// Logger config
+type Logger struct {
+	Development       bool
+	DisableCaller     bool
+	DisableStacktrace bool
+	Encoding          string
+	Level             string
+}
+
+// Postgresql config
+type PostgresConfig struct {
+	PostgresqlHost     string
+	PostgresqlPort     string
+	PostgresqlUser     string
+	PostgresqlPassword string
+	PostgresqlDbname   string
+	PostgresqlSSLMode  bool
+}
+
+// Cookie config
+type Cookie struct {
+	Name     string
+	MaxAge   int
+	Secure   bool
+	HTTPOnly bool
+}
+
+// Session config
+type Session struct {
+	Prefix string
+	Name   string
+	Expire int
+}
+
+// Metrics config
+type Metrics struct {
+	URL         string
+	ServiceName string
+}
+
+// Store config
+type Store struct {
+	ImagesFolder string
+}
+
+// Load config file from given path
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.AllowEmptyEnv(false)
+	v.SetEnvPrefix("dp")
+	v.SetEnvKeyReplacer(strings.NewReplacer("_", "."))
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return v, nil
+}
+
+// Parse config file
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
